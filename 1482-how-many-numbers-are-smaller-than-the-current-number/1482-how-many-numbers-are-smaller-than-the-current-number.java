b31fdaf6196d2df86928c159d5c3c78302f9c56e
Nitin <91310284+nitin-pandita@users.noreply.github.com>
2023-08-25 11:00:40 +0530

    Time: 9 ms (58.02%), Space: 43.3 MB (93.8%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int count = 0;
-        int [] res = new int[nums.length];
-        int k = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[i] > nums[j]) {
-                    count++;
+        int count = 0; // Initialize a variable to keep track of the count of smaller numbers
+        int[] res = new int[nums.length]; // Create an array to store the result
+        
+        // Loop through each element in the input array
+        for (int i = 0; i < nums.length; i++) {
+            // For each element, compare it with every other element in the array
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    count++; // Increment the count if the current element is greater than the compared element
                 }
             }
-            res[i] = count;
-            count =0;
+            res[i] = count; // Store the final count of smaller numbers for the current element in the result array
+            count = 0; // Reset the count for the next iteration
         }
-        return res;
+        
+        return res; // Return the array containing the count of smaller numbers for each element
     }
 }
